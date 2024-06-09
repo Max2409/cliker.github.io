c804383dea9b6ce152d92993fc3e7898c7ed1cb1
Max2409 <rydpr24@gmail.com>
2024-06-09 23:11:18 +0300

    Delete web directory

--- web/static/js/script.js
+++
@@ -1,7 +0,0 @@
-Telegram.WebApp.ready();
-
-document.querySelectorAll('.square').forEach(square => {
-    square.addEventListener('click', () => {
-        square.classList.toggle('clicked');
-    });
-});
