c76f0fe15c2a11cb2bcc94bedd0c39db4e04b954
Max2409 <rydpr24@gmail.com>
2024-06-10 00:33:18 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,16 @@
-Telegram.WebApp.ready();
+// Initial number of plots
+let numPlots = 4;
 
-document.querySelectorAll('.square').forEach(square => {
-    square.addEventListener('click', () => {
-        square.classList.toggle('clicked');
-    });
-});
+// Function to add a new plot
+function addPlot() {
+    if (numPlots < 8) {
+        numPlots++;
+        let newPlot = document.createElement('div');
+        newPlot.classList.add('plot');
+        newPlot.setAttribute('id', `plot${numPlots}`);
+        document.querySelector('.farm').appendChild(newPlot);
+    }
+}
+
+// Add event listener for the "Add Plot" button
+document.getElementById('addPlotButton').addEventListener('click', addPlot);
